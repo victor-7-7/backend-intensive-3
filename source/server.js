fe00cd19b2e6c51b9ca2ee3f192abaa9ee390696
victor-7-7 <Victor.7.7@ya.ru>
2021-01-22 15:40:41 +0700

    lesson-11. Implement task-1

--- source/server.js
+++ source/server.js
@@ -11,7 +11,15 @@ const app = express();
 const logger = winston.createLogger({
     level:      'debug',
     format:     winston.format.simple(),
-    transports: [ new winston.transports.Console() ],
+    transports: [
+        // Write all logs with level `error` and below to `error.log`
+        new winston.transports.File({
+            filename: './logs/errors.log',
+            level:    'error',
+        }),
+        // Write all logs with level `debug` and below to console
+        new winston.transports.Console(),
+    ],
 });
 
 app.use(bodyParser.json({ limit: '10kb' }));
@@ -29,11 +37,19 @@ app.use((req, res, next) => {
           new Date().toDateString() -> Wed Jan 13 2021
           new Date().toLocaleString() -> Wed Jan 13 2021 22:28:29 GMT+0700 (GMT+07:00)
           new Date().toUTCString() -> Wed, 13 Jan 2021 15:42:38 GMT
+          new Date().toISOString() -> 2021-01-13T15:43:11.834Z
           new Date().getTime() -> 1610552701183 <- ms since 01.01.70 00:00:00
           Date.now() -> 1610552701183 <- ms since 01.01.70 00:00:00
         */
     }
     next();
+});
+
+app.use('/', (error, req, res, next) => {
+    if (process.env.NODE_ENV !== 'test') {
+        logger.error(`${new Date().toISOString()} ${error.name}: ${error.message}`);
+    }
+    next(error);
 });
 
 // Routers
